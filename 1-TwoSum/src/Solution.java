8dd2464b609074cb649f0616a20de570252085f9
jsolorzanoc <jsolorzanoc08@outlook.com>
2026-03-04 22:20:34 -0600

    My final solution to twosum

--- 1-TwoSum/src/Solution.java
+++ 1-TwoSum/src/Solution.java
@@ -3,17 +3,25 @@ import java.util.Map;
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> pairIdx = new HashMap<>();
+        Map<Integer, Integer> notebook = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++){
+        for (var i = 0; i < nums.length; i++){
             int num = nums[i];
-            if (pairIdx.containsKey(target - num)){
-                return new int[] {i, pairIdx.get(target-num)};
+            if (notebook.containsKey(target - num)){
+                return new int[]{i, notebook.get(target-num)};
             }
-            pairIdx.put(num, i);
+            notebook.put(num,i);
         }
-
         return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] result1 = solution.twoSum(new int[]{2,7,11,15}, 9);
+        System.out.println("[" + result1[0] + "," + result1[1]+"]");
 
     }
+
+
+
 }
